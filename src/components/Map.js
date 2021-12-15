51b1064aa7a22e85de23303e233d033a5e54f8f7
kennedyc <charles.kennedy@computershare.co.uk>
2021-12-15 17:54:24 +0000

    Updated map to one with group labels.

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Map() {
 
     return (
-        <img src="./Scotland-map.png">
+        <img src="./Scotland-map-v3.png">
         </img>
     );
 }

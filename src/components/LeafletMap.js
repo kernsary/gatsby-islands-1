49e6f5d31b1c143edc1db7e03180e1e1a7bddf76
kennedyc <charles.kennedy@computershare.co.uk>
2021-12-15 17:24:17 +0000

    Added Map component with new image. Also added LeafletMap component, though unused currently.

---
+++ src/components/LeafletMap.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'gatsby-plugin-react-leaflet';
+import Map from 'gatsby-plugin-react-leaflet';
+import TileLayer from 'gatsby-plugin-react-leaflet';
+import Marker from 'gatsby-plugin-react-leaflet';
+import Popup from 'gatsby-plugin-react-leaflet';
+
+
+class LeafletMap extends React.Component {
+
+
+    static propTypes = {
+        /** Latitude and Longitude of the map centre in an array, eg [51, -1] **/
+        position: PropTypes.array,
+
+
+        /** Initial zoom level for the map (default 13) **/
+        zoom: PropTypes.number,
+
+
+        /** If set, will display a marker, which when clicked will display this text **/
+        markerText: PropTypes.string
+    }
+
+
+    static defaultProps = {
+        position: [51, -1],
+        zoom: 13,
+        markerText: ""
+    }
+
+    render() {
+        return (
+            <Map center={this.props.position} zoom={this.props.zoom}>
+                <TileLayer
+                    url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                {this.props.markerText !== "" &&
+                <Marker position={this.props.position}>
+                    <Popup>{this.props.markerText}</Popup>
+                </Marker>
+                }
+            </Map>
+        );
+    }
+}
+
+export default LeafletMap
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import RegionList from '../components/RegionList'
+import LeafletMap from '../components/LeafletMap'
+import Map from '../components/Map'
 
 // import styles from './index.module.css';
 
@@ -10,6 +12,17 @@ export default function IndexPage() {
   return (
     <Layout>
         <RegionList/>
+
+        {/*{typeof window !== 'undefined' &&*/}
+        {/*<LeafletMap*/}
+        {/*    position={[52,-0.5]}*/}
+        {/*    zoom={8}*/}
+        {/*    markerText={"Hello, this is a marker"}*/}
+        {/*/>*/}
+        {/*}*/}
+
+        <Map/>
+
     </Layout>
   );
 }
